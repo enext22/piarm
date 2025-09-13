3cbbc0d2770a6f4937547de5f546fe11d7ba169c
Emily <e.niska.edwards@gmail.com>
2025-09-13 18:30:55 -0400

    Adding realtime visualization using matplotlib to show ultrasonic sensor data

--- haptic_feedback_control/src/ultrasonic.py
+++ haptic_feedback_control/src/ultrasonic.py
@@ -2,13 +2,47 @@
 
 from robot_hat import Ultrasonic, Pin
 from time import sleep
+from matplotlib import pyplot, animation
+import datetime as dt
 
 trig = Pin("D0")
 echo = Pin("D1")
 
 hc_sr04 = Ultrasonic(trig, echo)
 
-while True:
-    val = hc_sr04.read()
-    print(val)
-    sleep(0.1)
+
+fig = pyplot.figure()
+ax = fig.add_subplot(1, 1, 1)
+time = []
+data = []
+
+def animate_readings(i, time, data):
+
+    dist = hc_sr04.read()
+
+    time.append(dt.datetime.now().strftime('%S.%f'))
+    data.append(dist)
+
+    # SLIDING WINDOW FOR PLOTS
+    time = time[-20:]
+    data = data[-20:]
+
+    ax.clear()
+    ax.plot(time, data)
+
+    # FORMATTING
+    pyplot.xticks(rotation=45, ha='right')
+    pyplot.subplots_adjust(bottom=0.30)
+    pyplot.title('HC-SR04 Distance over Time')
+    pyplot.ylabel('Distance (cm)')
+    pyplot.ylim(0, 20) #define fixed limits for graph to aid in visualization
+
+
+animated = animation.FuncAnimation(fig, animate_readings, fargs=(time, data), interval=1000)
+pyplot.show()
+
+#while True:
+#    val = hc_sr04.read()
+#    animate_readings(time, data, val)
+#    print(val)
+#    sleep(1)
